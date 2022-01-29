0bd74362a61d155db8c196ec3c322b68be33ff53
Maciej Czech <maciejek052@gmail.com>
2022-01-29 23:55:49 +0100

    wyświetlanie losowego albumu na stronie głównej

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Album} from "../album/album.model";
+import {AlbumService} from "../album/album.service";
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  albumy :Album[] = [];
+  losowyAlbum!: Album;
+
+  constructor(private albumyService: AlbumService) {
+    this.albumyService.pobierzAlbumy().subscribe(data => {
+      this.albumy = data;
+      var ktory_album = Math.floor(Math.random() * this.albumy.length);
+      this.losowyAlbum = this.albumy[ktory_album];
+    })
+  }
 
   ngOnInit(): void {
   }
