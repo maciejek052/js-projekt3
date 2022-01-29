c3931eb22b51d788030d27c1983cf586b462691d
Maciej Czech <maciejek052@gmail.com>
2022-01-29 22:01:21 +0100

    sortowanie wykonawców i albumów

--- src/app/album/album-przeglad/album-przeglad.component.ts
+++ src/app/album/album-przeglad/album-przeglad.component.ts
@@ -24,12 +24,21 @@ export class AlbumPrzegladComponent implements OnInit {
     this.id_wybranego_albumu_do_usuniecia.emit(id);
   }
 
-  sortujAlbumyAlfabetycznie() {
+  czySortowanoAlfabetycznie: boolean = false;
+  czySortowanoPoDacie: boolean = false;
 
+  sortujAlbumyAlfabetycznie() {
+    this.czySortowanoAlfabetycznie == false ? this.czySortowanoAlfabetycznie = true : this.czySortowanoAlfabetycznie = false;
+    this.tablica_albumow_do_wyswietlenia.sort((a,b) => a.name.localeCompare(b.name))
+    if (!this.czySortowanoAlfabetycznie)
+      this.tablica_albumow_do_wyswietlenia.reverse()
   }
 
   sortujAlbumyPoDacie() {
-    
+    this.czySortowanoPoDacie == false ? this.czySortowanoPoDacie = true : this.czySortowanoPoDacie = false;
+    this.tablica_albumow_do_wyswietlenia.sort((a,b) => a.yearOfRelease > b.yearOfRelease ? 1 : -1);
+    if (!this.czySortowanoPoDacie)
+      this.tablica_albumow_do_wyswietlenia.reverse()
   }
 
 }
--- src/app/wykonawca/wykonawca-przeglad/wykonawca-przeglad.component.ts
+++ src/app/wykonawca/wykonawca-przeglad/wykonawca-przeglad.component.ts
@@ -30,4 +30,21 @@ export class WykonawcaPrzegladComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  czySortowanoPoWykonawcy: boolean = false;
+  czySortowanoPoKraju: boolean = false;
+
+  sortujPoWykonawcy() {
+    this.czySortowanoPoWykonawcy == false ? this.czySortowanoPoWykonawcy = true : this.czySortowanoPoWykonawcy = false;
+    this.tablica_wykonawcow_do_wyswietlenia.sort((a,b) => a.name.localeCompare(b.name))
+    if (!this.czySortowanoPoWykonawcy)
+      this.tablica_wykonawcow_do_wyswietlenia.reverse()
+  }
+
+  sortujPoKraju() {
+    this.czySortowanoPoKraju == false ? this.czySortowanoPoKraju = true : this.czySortowanoPoKraju = false;
+    this.tablica_wykonawcow_do_wyswietlenia.sort((a,b) => a.country.localeCompare(b.country))
+    if (!this.czySortowanoPoKraju)
+      this.tablica_wykonawcow_do_wyswietlenia.reverse()
+  }
+
 }
